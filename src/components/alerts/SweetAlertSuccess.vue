f02049235f0054ee1d72fd74e9acd78698072f80
Vitor Pimenta <vitor.pimenta@mouts.info>
2023-08-09 18:20:58 -0300

    Inclui nos cookies as equipes que pertence, cria teste e mostra alerts

---
+++ src/components/alerts/SweetAlertSuccess.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+import Swal from "sweetalert2";
+
+export default {
+    name: 'SweetAlertSuccess',
+    mounted(){
+      Swal.fire({
+        icon: 'success',
+        title: 'Operação realizada com sucesso.',
+        showConfirmButton: false,
+        timer: 1000
+      })
+    }
+}
+</script>
